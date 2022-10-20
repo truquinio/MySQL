a5bd8e69587ab79ada3bbefc2df4e9855f404280
{ Fede Trucco } <103152265+truquinio@users.noreply.github.com>
2022-10-19 21:44:30 -0300

    19/10/2022

--- EjerciciosComunes/Ej03_Jugadores_NBA.sql
+++ EjerciciosComunes/Ej03_Jugadores_NBA.sql
@@ -2,7 +2,6 @@
 Abrir el script de la base de datos llamada “nba.sql” y ejecutarlo para crear todas las tablas e 
 insertar datos en las mismas. A continuación, generar el modelo de entidad relación. Deberá 
 obtener un diagrama de entidad relación igual al que se muestra a continuación: 
-
 A continuación, se deben realizar las siguientes consultas sobre la base de datos:
 */
 USE nba;
@@ -110,18 +109,18 @@ WHERE jug.nombre_equipo = eqp.nombre
 GROUP BY eqp.nombre;
 
 #	12.  Mostrar el jugador que más puntos ha realizado en toda su carrera.
-SELECT nombre
-FROM jugador
+SELECT distinct nombre
+FROM jugador, estadistica
 WHERE codigo = (SELECT jugador
                 FROM estadistica
                 GROUP BY jugador
-                HAVING SUM(Puntos_por_partido) = (SELECT MAX(sumas.Total)
-                                                  FROM (SELECT SUM(Puntos_por_partido) AS 'Total'
+                HAVING sum(Puntos_por_partido) = (SELECT max(sumas.Total)
+                                                  FROM (SELECT sum(Puntos_por_partido) AS 'Total'
                                                         FROM estadistica
                                                         GROUP BY jugador) AS sumas));
 
 #	FORMA 2:
-SELECT suma.nombre, suma.puntos
+SELECT suma.nombre, round(suma.puntos)
 FROM (SELECT jug.nombre, sum(est.Puntos_por_partido) AS 'puntos'
       FROM jugador jug, estadistica est
       WHERE jug.codigo = est.jugador
@@ -132,18 +131,43 @@ WHERE suma.puntos = (SELECT max(suma2.puntos) FROM (SELECT jug.nombre, sum(est.P
                                                     WHERE jug.codigo = est.jugador
                                                     GROUP BY jug.nombre) suma2);
 
+SELECT nombre AS 'Jugador', round(sum(Puntos_por_partido)) AS 'Puntos totales'								
+FROM estadistica estad, jugador jug
+WHERE estad.jugador = jug.codigo 
+AND lower(jug.nombre) = 'Shaquille ONeal';
 
+#	13.  Mostrar el nombre del equipo, conferencia y división del jugador más alto de la NBA.
+SELECT jug.nombre AS 'jugador', jug.altura, eqp.nombre AS equipo, eqp.conferencia, eqp.division 
+FROM equipo eqp, jugador jug
+WHERE eqp.nombre = jug.nombre_equipo 
+AND jug.altura = (SELECT max(altura) 
+					FROM jugador);
 
+#	14.  Mostrar la media de puntos en partidos de los equipos de la división Pacific.
+SELECT part.equipo_local, AVG(part.puntos_local) as 'Media_de_puntos', eqp.division
+FROM partido as part
+INNER JOIN equipo as eqp
+ON part.equipo_local = eqp.nombre
+WHERE eqp.division = 'Pacific'
+GROUP BY part.equipo_local;
 
-/*
+#	15. Mostrar el partido o partidos (equipo_local, equipo_visitante y diferencia) con mayor diferencia de puntos.
+SELECT *
+FROM partido
+WHERE abs(puntos_local - puntos_visitante) = (SELECT max(abs(puntos_local - puntos_visitante)) FROM partido);
 
+#	16.  Mostrar la media de puntos en partidos de los equipos de la división Pacific.
+SELECT eqp.nombre, AVG(puntos_local) AS 'Promedio puntos', eqp.division
+FROM partido as part
+INNER JOIN equipo as eqp
+ON part.equipo_local = eqp.nombre
+WHERE eqp.division = 'Pacific'
+GROUP BY eqp.nombre;
 
+#	17.  Mostrar los puntos de cada equipo en los partidos, tanto de local como de visitante. 
+SELECT equipo_local, puntos_local, equipo_visitante, puntos_visitante FROM partido;
 
-13.  Mostrar el nombre del equipo, conferencia y división del jugador más alto de la NBA.
-14.  Mostrar la media de puntos en partidos de los equipos de la división Pacific.
-15.  Mostrar el partido o partidos (equipo_local, equipo_visitante y diferencia) con mayor 
-diferencia de puntos.
-16.  Mostrar la media de puntos en partidos de los equipos de la división Pacific.
-17.  Mostrar los puntos de cada equipo en los partidos, tanto de local como de visitante. 
-18.  Mostrar quien gana en cada partido (codigo, equipo_local, equipo_visitante, equipo_ganador), en caso de empate sera null.
-*/
+#	18.  Mostrar quien gana en cada partido (codigo, equipo_local, equipo_visitante, equipo_ganador), 
+#	en caso de empate sera null.
+SELECT codigo, equipo_local, equipo_visitante, IF(puntos_local>puntos_visitante, equipo_local, IF(puntos_local<puntos_visitante, equipo_visitante, null)) as 'equipo_ganador'
+FROM partido;
